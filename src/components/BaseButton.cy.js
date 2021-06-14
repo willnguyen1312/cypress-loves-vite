89b4a0c2d586ecb03b45975c3851fcc71ff54a70
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-14 16:16:51 +0700

    Stuff

--- src/components/BaseButton.cy.js
+++ src/components/BaseButton.cy.js
@@ -1,14 +1,15 @@
-import { mount } from '@cypress/vue'
-import ExampleBaseButton from './ExampleBaseButton.vue'
-import BaseButton from './BaseButton.vue'
+import { mount } from "@cypress/vue";
+import ExampleBaseButton from "./ExampleBaseButton.vue";
+import BaseButton from "./BaseButton.vue";
 
-describe('BaseButton', () => {
-  it('playground', () => {
-    mount(ExampleBaseButton)
-  })
+describe("BaseButton", () => {
+  it("playground", () => {
+    mount(ExampleBaseButton);
+  });
 
-  it('takes in the default slot', () => {
-    mount(BaseButton, { slots: { default: 'Hello' }
-    }).get('button').should('have.text', 'Hello')
-  })
-})
+  it("takes in the default slot", () => {
+    mount(BaseButton, { slots: { default: "Hello" } })
+      .get("button")
+      .should("have.text", "Hello");
+  });
+});
